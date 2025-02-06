b5b4889124b68b4402def1d54ecd44e5e8ee2317
Tibo1310 <thibaultdtpro@gmail.com>
2025-02-06 15:48:24 +0100

    api json

--- api.php
+++ api.php
@@ -7,6 +7,10 @@ error_log("Requête reçue : " . $_SERVER['REQUEST_METHOD']);
 
 $userManager = new UserManager();
 
+// Récupérer les données JSON
+$rawData = file_get_contents('php://input');
+$data = json_decode($rawData, true);
+
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     error_log("GET request");
     echo json_encode($userManager->getUsers());
@@ -15,20 +19,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     error_log("POST request");
-    $rawData = file_get_contents('php://input');
     error_log("Données reçues : " . $rawData);
-    
-    $data = json_decode($rawData, true);
     
     try {
         switch ($data['action']) {
             case 'add':
                 error_log("Action: add");
-                $userManager->addUser($data['name'], $data['email']);
+                $userManager->addUser($data['name'], $data['email'], $data['role']);
                 break;
             case 'update':
                 error_log("Action: update");
-                $userManager->updateUser($data['id'], $data['name'], $data['email']);
+                $userManager->updateUser($data['id'], $data['name'], $data['email'], $data['role']);
                 break;
             case 'delete':
                 error_log("Action: delete");
@@ -39,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     } catch (Exception $e) {
         error_log("Erreur : " . $e->getMessage());
         http_response_code(400);
-        echo $e->getMessage();
+        echo json_encode(['error' => $e->getMessage()]);
     }
     exit;
 }
